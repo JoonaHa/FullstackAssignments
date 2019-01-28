bdeccfd9e3eb9fbc96533c4bc24ba714c3cc3488
JoonaHa <joona.halonen@helsinki.fi>
2019-01-29 01:52:32 +0200

    1.14*

--- Part1/anekdootit/src/index.js
+++ Part1/anekdootit/src/index.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
+const Header =({text}) => (
+    <h1>{text}</h1>
+  )
+  
 const Button = ({handleClick, text}) => {
     return (
         <button onClick={handleClick}>
         {text}
         </button>
+    )
+}
+
+const BestAnecdote = ({anecdotes, points}) => {
+      var max = points[0]
+      var maxIndex = 0;
+      
+      for (var i = 1; i < points.length; i++) {
+        if (points[i] > max) {
+            maxIndex = i;
+            max = points[i];
+        }
+    }
+  
+    console.log(anecdotes[maxIndex])
+    return (
+        <>
+        <p>
+            {anecdotes[maxIndex]}
+        </p>
+        <h4>
+            Anecdote has {max} points
+        </h4>
+        </>
     )
 }
 
@@ -17,11 +46,20 @@ const App = (props) => {
    setSelected(random)
   }
 
+  const handleVote = () => {
+    props.points[selected]++
+    console.log(selected,'votes',props.points[selected])
+   }
+
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <Button handleClick={handleClick} text='next anecdote'/>
+      <Header text='Anecdote of the day'/>
+      <div><p>{props.anecdotes[selected]}</p></div> 
+      <Button handleClick={handleClick} text='Next anecdote'/>
+      <Button handleClick={handleVote} text='Vote'/>
+      <Header text='Anecdote with the most votes'/>
+      <BestAnecdote anecdotes={props.anecdotes} points={props.points}/>
     </div>
   )
 }
@@ -35,8 +73,10 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+let points = Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0);
+
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App anecdotes={anecdotes} points={points} />,
   document.getElementById('root')
 )
