94b79bdd534289406012b4ff983ff7ec62eb45c1
JoonaHa <joona.halonen@helsinki.fi>
2019-02-11 23:33:07 +0200

    3.11

---
+++ Part3/puhelinluettelo/build/precache-manifest.4c492b33b7fdf4290bea9d34f8c840a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a024b5765fd99ca61d60",
+    "url": "/static/js/main.a024b576.chunk.js"
+  },
+  {
+    "revision": "689475fda52e7f930071",
+    "url": "/static/js/1.689475fd.chunk.js"
+  },
+  {
+    "revision": "a024b5765fd99ca61d60",
+    "url": "/static/css/main.3115821b.chunk.css"
+  },
+  {
+    "revision": "9c7b8378ea6b8aa013b8b19632570d66",
+    "url": "/index.html"
+  }
+];
