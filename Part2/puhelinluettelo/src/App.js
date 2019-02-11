065a5f5869f7f552d0c0dd049bb7d1309a675c20
JoonaHa <joona.halonen@helsinki.fi>
2019-02-11 23:08:58 +0200

    3.9

--- Part2/puhelinluettelo/src/App.js
+++ Part2/puhelinluettelo/src/App.js
@@ -1,116 +1,172 @@
 import React, { useState, useEffect } from 'react'
-import Number from './Components/Number.js'
-import axios from 'axios'
+import personService from './services/persons'
 
-const App = () => {
-  const [persons, setPersons] = useState([   { name: 'Arto Hellas', number: '040-123456' },
-  { name: 'Martti Tienari', number: '040-123456' },
-  { name: 'Arto Järvinen', number: '040-123456' },
-  { name: 'Lea Kutvonen', number: '040-123456' }])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [filterValue, setFilterValue] = useState('')
-  const [useFilter, setUseFilter] = useState(false)
-
-  const numbersToShow = !useFilter
-  ? persons
-  : persons.filter(e => e.name.toLowerCase().includes(filterValue.toLocaleLowerCase()))
-
-  const CheckDuplicate = () => {
-    if (persons.some(e => e.name === newName)) {
-      return true
-    }
-    return false
+const Notification = ({ notification }) => {
+  if (notification.message === null) {
+    return null
   }
 
+  const style = {
+    color: notification.type === 'error' ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+
+  return (
+    <div style={style}>
+      {notification.message}
+    </div>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <div>
+      rajaa näytettäviä
+      <input onChange={props.handleChange} value={props.value} />
+    </div>
+  )
+}
+
+const Persons = (props) => {
+  return (
+    props.persons.map(p =>
+      <div key={p.name}>
+        {p.name} {p.number} <button onClick={()=>props.deletePerson(p.id)}>poista</button>
+      </div>
+    )
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        nimi: <input onChange={props.handleNameChange} value={props.newName} />
+      </div>
+      <div>
+        numero: <input onChange={props.handleNumberChange} value={props.newNumber} />
+      </div>
+      <div>
+        <button type="submit">lisää</button>
+      </div>
+    </form>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState({
+    message: null
+  })
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService.getAll()
+      .then(data => {
+        setPersons(data)
       })
   }, [])
 
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
-
-  const Duplicate= () => CheckDuplicate()
-
-  const numbers = () => numbersToShow.map(number =>
-    <Number
-      key={number.name}
-      person={number}
-    />
-  )
-
-
-
-  const addPerson = (event) => {
+  const notify = (message, type='success') => {
+    setNotification({ message, type })
+    setTimeout(() => setNotification({ message: null }), 10000)
+  }
+ 
+  const handleSubmit = (event) => {
     event.preventDefault()
-    if (Duplicate()) {
-      return alert(`${newName} on jo luettelossa`)
+
+    const existingPerson = persons.find(p => p.name === newName)
+
+    if (existingPerson) {
+      const ok = window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella`)
+      
+
+      
+      if (ok) {
+        personService
+          .replace({
+            ...existingPerson,
+            number: newNumber
+          })
+          .then(replacedPerson => {
+            setPersons(persons.map(p => p.name === newName ? replacedPerson : p))
+            setNewName('')
+            setNewNumber('')
+            notify(`Henkilön ${newName} numero muutettu`)
+          })
+          /*
+          .catch(() => {
+            setPersons(persons.filter(p => p.name !== newName))
+            notify(`Henkilön ${newName} oli jo poistettu`, 'error')
+          })
+          */
+      }
+
+      return
+    } 
+     
+  personService
+      .create({
+        name: newName,
+        number: newNumber
+      })
+      .then(createdPerson => {
+        setPersons(persons.concat(createdPerson))
+        setNewName('')
+        setNewNumber('')
+        notify(`Lisättiin ${createdPerson.name}`)
+      })
+  }
+
+  const deletePerson = (id) => {
+    const person = persons.find(p => p.id === id)
+    const ok = window.confirm(`Poistetaanko ${person.name}`)
+    if (ok) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+        })
+      notify(`Poistettiin ${person.name}`)
     }
-    const personObject = {
-      name: newName,
-      number: newNumber 
-    }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-
   }
 
-  const handleNameFormChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberFormChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-
-  const handleFilterChange = (event) => {
-    if (event.target.value !== '') {
-      setFilterValue(event.target.value)
-      setUseFilter(true)
-    }  else {
-      setFilterValue(event.target.value)
-      setUseFilter(false)
-    }
-  }
+  const personsToShow = filter.length === 0
+    ? persons 
+    : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()) )
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <form>
-        <div>
-          rajaa näytettäviä:  <input
-          value={filterValue}
-          onChange={handleFilterChange}
-          />
-        </div>
-      </form>
-      <form onSubmit={addPerson}>
-      <h3>Lisää uusi</h3>
-        <div>
-          nimi: <input value = {newName}
-                  onChange={handleNameFormChange}
-                />
-        </div>
-        <div>numero: 
-          <input value={newNumber}
-            onChange={handleNumberFormChange}
-          />
-        </div>
-        <div>
-          <button type="submit">lisää</button>
-        </div>
-      </form>
-      <h3>Numerot:</h3>
-      <ul>
-        {numbers()}
-      </ul>
+
+      <Notification notification={notification} />
+
+      <Filter handleChange={handleFilterChange} value={filter} />
+
+      <h3>lisää uusi</h3>
+
+      <PersonForm 
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleSubmit={handleSubmit}
+        newName={newName}
+        newNumber={newNumber}
+      />
+
+      <h3>Numerot</h3>
+
+      <Persons persons={personsToShow} deletePerson={deletePerson} />
     </div>
   )
 
