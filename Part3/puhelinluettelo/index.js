c07078e2d14cae47147840eb4064807336f0c6df
JoonaHa <joona.halonen@helsinki.fi>
2019-02-11 19:47:48 +0200

    3.3

--- Part3/puhelinluettelo/index.js
+++ Part3/puhelinluettelo/index.js
@@ -37,6 +37,17 @@ let persons = [
      res.send(content)
   })
 
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    console.log(person)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
