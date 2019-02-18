77e8a43ea85a785ed837932cb7597e9cb386f970
JoonaHa <joona.halonen@helsinki.fi>
2019-02-18 22:40:18 +0200

    4.4

--- Part4/blogilistasovellus/utils/list_helper.js
+++ Part4/blogilistasovellus/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
    return 1
   }
+
+  const totalLikes = (blogs) => {
+    return blogs.reduce((add, blog) => add + blog.likes,0)
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
+
